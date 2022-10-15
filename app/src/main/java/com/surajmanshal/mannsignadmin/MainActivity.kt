4f1d870c5a326099b2e2b635e1b5aa9dd4717191
Manshal Khatri <khatrimanshal1206@gmail.com>
2022-10-15 16:27:33 +0530

    Added Button to Navigate on ProductManagementActivity.kt

--- app/src/main/java/com/surajmanshal/mannsignadmin/MainActivity.kt
+++ app/src/main/java/com/surajmanshal/mannsignadmin/MainActivity.kt
@@ -19,8 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        startActivity(Intent(this,ProductManagementActivity::class.java))
-
 
         setupViewPager()
     }
