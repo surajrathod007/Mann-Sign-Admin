dd0e572f4ca211bb347dcc74353976256c050148
Manshal Khatri <khatrimanshal1206@gmail.com>
2023-01-28 09:22:52 +0530

    Added Product Searching feature

--- app/src/main/java/com/surajmanshal/mannsignadmin/ui/fragments/ProductsListFragment.kt
+++ app/src/main/java/com/surajmanshal/mannsignadmin/ui/fragments/ProductsListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
@@ -14,6 +15,8 @@ import com.surajmanshal.mannsignadmin.data.model.product.Product
 import com.surajmanshal.mannsignadmin.databinding.FragmentProductsListBinding
 import com.surajmanshal.mannsignadmin.ui.activity.ProductManagementActivity
 import com.surajmanshal.mannsignadmin.ui.activity.ProductsActivity
+import com.surajmanshal.mannsignadmin.utils.hide
+import com.surajmanshal.mannsignadmin.utils.show
 import com.surajmanshal.mannsignadmin.viewmodel.ProductsViewModel
 
 class ProductsListFragment : Fragment() {
@@ -23,12 +26,18 @@ class ProductsListFragment : Fragment() {
     lateinit var mVM : ProductsViewModel
     lateinit var mActivity: ProductsActivity
     var columCount = 2
+    var lastQuery = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
         }
+        mVM.products.observe(requireActivity(), Observer {
+            if(binding.rvProducts.adapter == null)
+                setAdapterWithList(it)
+        })
+        mVM.getPosters()
     }
 
     override fun onCreateView(
@@ -47,10 +56,41 @@ class ProductsListFragment : Fragment() {
                 activity?.finish()
             }
         }
-        mVM.products.observe(viewLifecycleOwner, Observer {
-            setAdapterWithList(it)
-        })
-        mVM.getPosters()
+
+        with(binding) {
+            etSearch.setOnQueryTextListener(object :
+                androidx.appcompat.widget.SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    Toast.makeText(requireContext(), "$query", Toast.LENGTH_SHORT).show()
+                    val resultList = getSearchedProducts(query)
+                    if (resultList.isNullOrEmpty()) {
+                        tvNoProducts.show()
+                        rvProducts.hide()
+                        return true
+                    }
+                    tvNoProducts.hide()
+                    rvProducts.show()
+                    setAdapterWithList(resultList)
+                    lastQuery = query!!
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if(newText.isNullOrBlank()){
+                        tvNoProducts.hide()
+                        rvProducts.show()
+                        mVM.products.value?.let { setAdapterWithList(it) }
+                    }
+                    return false
+                }
+            })
+            getSearchedProducts(
+                lastQuery
+            )?.let {
+                setAdapterWithList(it)
+            }
+        }
+
         return view
     }
 
@@ -58,7 +98,15 @@ class ProductsListFragment : Fragment() {
         if (list.isNotEmpty()) binding.rvProducts.adapter = ProductsAdapter(list.reversed(),mActivity)
     }
 
-
+    fun getSearchedProducts(query : String?): List<Product>? {
+        return mVM.products.value?.filter {
+            query?.let { it1 ->
+                it.productId.toString().contains(it1) or
+                it.posterDetails?.title.toString().contains(it1,true) or
+                it.productCode.toString().contains(it1,true)
+            } == true
+        }
+    }
     companion object {
 
         @JvmStatic
